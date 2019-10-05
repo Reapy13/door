df492e7ec089ab0a611e239444dc226669c56c5c
Reapy13 <hugo.fauchery@gmail.com>
2019-10-05 17:07:18 +0200

    feat(#2) : Creates footer-banner and places it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeadBannerComponent } from './components/head-banner/head-banner.component';
+import { FooterBannerComponent } from './components/footer-banner/footer-banner.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadBannerComponent
+    HeadBannerComponent,
+    FooterBannerComponent
   ],
   imports: [
     BrowserModule
